978a7aaa5cd07e01aa028b9588106e1d2f6893d6
isaac-cminor <isaacmwesigwa7@gmail.com>
2017-09-22 06:58:39 +0300

    initial commit

---
+++ countdown.py
@@ -0,0 +1,19 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> start =10
+>>> while start:
+	print (start)
+	start -=1
+
+	
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+>>> 
